db7f9ef451c602c0ba0356c9acf68cd7ca338ba7
desanctis-n <desanctis.n@ufl.edu>
2024-06-14 23:37:20 -0400

    Home page altered. Style separated into standalone css file, image folder with mascot pngs

--- seele_app/urls.py
+++ seele_app/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from website.views import home,login,signup,familyinfo,profile,allusers,disasterprep,disasterchecklist,logout,singleuser,edituser,deleteuser
- 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
